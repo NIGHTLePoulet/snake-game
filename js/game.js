c3148632203719d1d2b8fd9bfb134182b0bc3204
Noah <marcotnoah@gmail.com>
2026-02-09 12:53:30 +0100

    Add snake movement with arrow key controls

--- js/game.js
+++ js/game.js
@@ -1,17 +1,52 @@
 const canvas = document.querySelector('#snake-grid')
 const ctx = canvas.getContext('2d')
 
-document.addEventListener('keydown', (e) => {
-    const rightArrow = e.key
+let direction = null 
+let x = 200
+let y = 200
+const size = 50
 
-    if (rightArrow === 'ArrowRight'){
-        console.log(rightArrow)
-    }
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') {
+        direction = 'right'
+    } 
+
+    if (e.key === 'ArrowDown') {
+        direction = 'up'
+    } 
+
+    if (e.key === 'ArrowLeft') {
+        direction = 'left'
+    } 
+
+    if (e.key === 'ArrowUp') {
+        direction = 'down'
+    } 
 })
 
-function draw() {
-    let snake = ctx.fillRect(0, 0, 50, 50)
-    return snake
+function animate() {
+    if (direction === 'right') {
+        x += 1
+    } 
+
+    if (direction === 'up') {
+        y += 1
+    } 
+
+    if (direction === 'left') {
+        x -= 1
+    } 
+
+    if (direction === 'down') {
+        y -= 1
+    } 
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'green'
+    ctx.fillRect(x, y, size, size)
+
+    requestAnimationFrame(animate)
 }
 
-draw()
+animate()
